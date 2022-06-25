66d8dbee1b7120a58b948e86ca0cdba95985950d
Harsh Garg <gargharsh2512@gmail.com>
2022-06-26 01:04:02 +0530

    Can do basic calculations
    This can add, subtract, multiply or divide to numbers of any length. Will have to clear the screen manually everytime to perform new action for now. Cannot do more than 1 action at a time.

--- script.js
+++ script.js
@@ -15,20 +15,106 @@ const numbers = [...document.querySelectorAll(".number")]
 
 const display = document.querySelector(".display");
 
+let expression = '';
+
 let mainButtons = [addButton, subtractButton, multiplyButton, divideButton];
+
+
 
 for(let i = 0; i < mainButtons.length; i++){
     mainButtons[i].addEventListener('click', addOnDisplay)
 };
 
 clearButton.addEventListener('click', () => {
-    display.textContent = '';
+    clearScreen();
+    expression = '';
 });
+
+equalButton.addEventListener('click', evaluateExp);
 
 for(let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener('click', addOnDisplay);
 };
 
+
+
+
 function addOnDisplay() {
-    display.textContent += this.textContent;
-}
+    display.textContent += this.textContent.trim();
+    addToExpression(this);
+};
+
+function clearScreen() {
+    display.textContent = '';
+};
+
+function addToExpression(element) {
+    if([...element.classList].includes('multiply')){
+        expression += '*';
+    }else if([...element.classList].includes('divide')){
+        expression += '/';
+    }else{
+        expression += element.textContent.trim();
+    };
+};
+
+
+function evaluateExp() {
+    let expArray = Array.from(expression);
+    let firstNum = '';
+    let secNum = '';
+    let action = '';
+    let actionReached = false;
+    let result = '';
+
+    for(let i = 0; i < expArray.length; i++){
+        
+        if (isNaN(+expArray[i])){
+            action = expArray[i];
+            actionReached = true;
+        }else if(actionReached == false){
+            firstNum += expArray[i];
+        }else{
+            secNum += expArray[i];
+        };
+
+    };
+
+    if(action == '' || firstNum == '' || secNum == ''){
+        result = 'Error';
+    }else if(action == '+'){
+        result = add(firstNum, secNum);
+    }else if(action == '-'){
+        result = subtract(firstNum, secNum);
+    }else if(action == '*'){
+        result = multiply(firstNum, secNum);
+    }else if(action == '/'){
+        result = divide(firstNum, secNum);
+    };
+
+    displayResult(result);
+};
+
+function add(a, b) {
+    return +a + +b;
+};
+
+function multiply(a, b){
+    return +a * +b;
+};
+
+function divide(a, b) {
+    if(b==0){
+        return 'Error';
+    }
+    return +a/+b;
+};
+
+function subtract(a, b) {
+    return +a - +b;
+};
+
+function displayResult(res) {
+    clearScreen();
+    display.textContent = res;
+};
